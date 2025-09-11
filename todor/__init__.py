23e6ddbd3d24128966a525762af95a9457ba5f78
Yus <yusdev05@gmail.com>
2025-09-11 10:41:29 -0400

    primer commit de todo-list

---
+++ todor/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+def create_app():
+    
+    app = Flask(__name__)
+    
+    
+    #configuracion del proyecto
+    app.config.from_mapping(
+        DEBUG = True,
+        SECRET_KEY = 'dev'
+    )
+    
+    
+    
+    #registrar Blueprint-----------
+    from . import todo
+    app.register_blueprint(todo.bp)
+    
+    from . import auth
+    app.register_blueprint(auth.bp)
+    #------------------------------
+    
+    
+    
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    
+    return app
+
+
+    
